b807db4e5a3ced59ec3e76cbe8ff675e491b63ae
jawblia <juliabell021@gmail.com>
2020-08-30 18:51:33 -0400

    Updates

---
+++ precache-manifest.d849eafb827f4b772f5a7a4bcf40d70b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce0f59dc66ff75a43f61ec49186881e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e21603f6ebc57f491ed",
+    "url": "/static/css/main.1b6892dc.chunk.css"
+  },
+  {
+    "revision": "d67e61e685511b30e6b7",
+    "url": "/static/js/2.679bb777.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/static/js/2.679bb777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e21603f6ebc57f491ed",
+    "url": "/static/js/main.1fc0f9c4.chunk.js"
+  },
+  {
+    "revision": "8f8bfba1f9ecdb1700e4",
+    "url": "/static/js/runtime-main.74785f10.js"
+  },
+  {
+    "revision": "b6db5d2b31900860292e6b34bc210791",
+    "url": "/static/media/Autopia-Regular.b6db5d2b.woff"
+  },
+  {
+    "revision": "85e8529a58875f89e90dfecbd69bea2c",
+    "url": "/static/media/Home.85e8529a.svg"
+  }
+]);
